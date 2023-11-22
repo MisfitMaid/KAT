79e35879d67081db4550b4ea2f372c1747fab6c1
jmack <jmack.contact@gmail.com>
2023-11-22 15:51:07 -0600

    Add grenadier config

---
+++ KAT_Armor/_base/armor/M56S_Jaeger/grenadier/config.cpp
@@ -0,0 +1,556 @@
+class CfgPatches
+{
+    class KAT_M56S_Jaeger_Armor_Grenadier
+    {
+        author = "wlan0";
+        addonRootClass = "KAT_M56S_Jaeger_Armor";
+        requiredAddons[] = 
+        {
+            "KAT_M56S_Jaeger_Armor"
+        };
+        requiredVersion = 0.1;
+        units[] = {};
+        weapons[] = {};
+    };
+};
+class CfgWeapons
+{
+    class KAT_M56S_Jaeger_Armor_Base;
+    class KAT_M56S_Jaeger_Armor_Rifleman : KAT_M56S_Jaeger_Armor_Base
+    {
+        class ItemInfo;
+    };
+    /* Default */
+    class KAT_M56S_Jaeger_Armor_Grenadier: KAT_M56S_Jaeger_Armor_Rifleman
+    {
+        displayName = "[KAT] M56S/J Grenadier";
+        hiddenSelections[] =
+        {
+            "camo",
+            "camo2",
+            "camo3",
+            "camo4",
+            "camo5",
+            "A_KneesMarLeft",
+            "A_KneesMarRight",
+            "A_Ghillie",
+            "A_TacPad",
+            "AS_LargeLeft",
+            "AS_LargeRight",
+            "AS_MediumLeft",
+            "AS_MediumRight",
+            "AS_ODSTCQBLeft",
+            "AS_ODSTCQBRight",
+            "AS_ODSTSniperLeft",
+            "AS_ODSTSniperRight",
+            "AS_SmallLeft",
+            "AS_SmallRight",
+            "AP_AR",
+            "AP_BR",
+            "AP_Canteen",
+            "AP_Knife",
+            "AP_MGThigh",
+            "AP_AR",
+            "AP_Pack",
+            "AP_Pistol",
+            "AP_Rounds",
+            "AP_SG",
+            "AP_SMG",
+            "AP_Sniper",
+            "AP_Thigh",
+            "APO_BR",
+            "APO_Knife",
+            "APO_SMG",
+            "APO_Sniper",
+            "CustomKit_Scorch"
+        };
+        class ItemInfo: ItemInfo
+        {
+            hiddenSelections[] =
+            {
+                "camo",
+                "camo2",
+                "camo3",
+                "camo4",
+                "camo5",
+                "A_KneesMarLeft",
+                "A_KneesMarRight",
+                "A_Ghillie",
+                "A_TacPad",
+                "AS_LargeLeft",
+                "AS_LargeRight",
+                "AS_MediumLeft",
+                "AS_MediumRight",
+                "AS_ODSTCQBLeft",
+                "AS_ODSTCQBRight",
+                "AS_ODSTSniperLeft",
+                "AS_ODSTSniperRight",
+                "AS_SmallLeft",
+                "AS_SmallRight",
+                "AP_AR",
+                "AP_BR",
+                "AP_Canteen",
+                "AP_Knife",
+                "AP_MGThigh",
+                "AP_AR",
+                "AP_Pack",
+                "AP_Pistol",
+                "AP_Rounds",
+                "AP_SG",
+                "AP_SMG",
+                "AP_Sniper",
+                "AP_Thigh",
+                "APO_BR",
+                "APO_Knife",
+                "APO_SMG",
+                "APO_Sniper",
+                "CustomKit_Scorch"
+            };
+        };
+    };
+    /* Red */
+    class KAT_M56S_Jaeger_Armor_Rifleman_Red : KAT_M56S_Jaeger_Armor_Rifleman { };
+    class KAT_M56S_Jaeger_Armor_Grenadier_Red : KAT_M56S_Jaeger_Armor_Rifleman_Red
+    {
+        displayName = "[KAT] M56S/J Grenadier [Red]";
+        hiddenSelections[] =
+        {
+            "camo",
+            "camo2",
+            "camo3",
+            "camo4",
+            "camo5",
+            "A_KneesMarLeft",
+            "A_KneesMarRight",
+            "A_Ghillie",
+            "A_TacPad",
+            "AS_LargeLeft",
+            "AS_LargeRight",
+            "AS_MediumLeft",
+            "AS_MediumRight",
+            "AS_ODSTCQBLeft",
+            "AS_ODSTCQBRight",
+            "AS_ODSTSniperLeft",
+            "AS_ODSTSniperRight",
+            "AS_SmallLeft",
+            "AS_SmallRight",
+            "AP_AR",
+            "AP_BR",
+            "AP_Canteen",
+            "AP_Knife",
+            "AP_MGThigh",
+            "AP_AR",
+            "AP_Pack",
+            "AP_Pistol",
+            "AP_Rounds",
+            "AP_SG",
+            "AP_SMG",
+            "AP_Sniper",
+            "AP_Thigh",
+            "APO_BR",
+            "APO_Knife",
+            "APO_SMG",
+            "APO_Sniper",
+            "CustomKit_Scorch"
+        };
+        class ItemInfo: ItemInfo
+        {
+            hiddenSelections[] =
+            {
+                "camo",
+                "camo2",
+                "camo3",
+                "camo4",
+                "camo5",
+                "A_KneesMarLeft",
+                "A_KneesMarRight",
+                "A_Ghillie",
+                "A_TacPad",
+                "AS_LargeLeft",
+                "AS_LargeRight",
+                "AS_MediumLeft",
+                "AS_MediumRight",
+                "AS_ODSTCQBLeft",
+                "AS_ODSTCQBRight",
+                "AS_ODSTSniperLeft",
+                "AS_ODSTSniperRight",
+                "AS_SmallLeft",
+                "AS_SmallRight",
+                "AP_AR",
+                "AP_BR",
+                "AP_Canteen",
+                "AP_Knife",
+                "AP_MGThigh",
+                "AP_AR",
+                "AP_Pack",
+                "AP_Pistol",
+                "AP_Rounds",
+                "AP_SG",
+                "AP_SMG",
+                "AP_Sniper",
+                "AP_Thigh",
+                "APO_BR",
+                "APO_Knife",
+                "APO_SMG",
+                "APO_Sniper",
+                "CustomKit_Scorch"
+            };
+        };
+    };
+    /* Blue */
+    class KAT_M56S_Jaeger_Armor_Rifleman_Blue : KAT_M56S_Jaeger_Armor_Rifleman { };
+    class KAT_M56S_Jaeger_Armor_Grenadier_Blue : KAT_M56S_Jaeger_Armor_Rifleman_Blue
+    {
+        displayName = "[KAT] M56S/J Grenadier [Blue]";
+        hiddenSelections[] =
+        {
+            "camo",
+            "camo2",
+            "camo3",
+            "camo4",
+            "camo5",
+            "A_KneesMarLeft",
+            "A_KneesMarRight",
+            "A_Ghillie",
+            "A_TacPad",
+            "AS_LargeLeft",
+            "AS_LargeRight",
+            "AS_MediumLeft",
+            "AS_MediumRight",
+            "AS_ODSTCQBLeft",
+            "AS_ODSTCQBRight",
+            "AS_ODSTSniperLeft",
+            "AS_ODSTSniperRight",
+            "AS_SmallLeft",
+            "AS_SmallRight",
+            "AP_AR",
+            "AP_BR",
+            "AP_Canteen",
+            "AP_Knife",
+            "AP_MGThigh",
+            "AP_AR",
+            "AP_Pack",
+            "AP_Pistol",
+            "AP_Rounds",
+            "AP_SG",
+            "AP_SMG",
+            "AP_Sniper",
+            "AP_Thigh",
+            "APO_BR",
+            "APO_Knife",
+            "APO_SMG",
+            "APO_Sniper",
+            "CustomKit_Scorch"
+        };
+        class ItemInfo: ItemInfo
+        {
+            hiddenSelections[] =
+            {
+                "camo",
+                "camo2",
+                "camo3",
+                "camo4",
+                "camo5",
+                "A_KneesMarLeft",
+                "A_KneesMarRight",
+                "A_Ghillie",
+                "A_TacPad",
+                "AS_LargeLeft",
+                "AS_LargeRight",
+                "AS_MediumLeft",
+                "AS_MediumRight",
+                "AS_ODSTCQBLeft",
+                "AS_ODSTCQBRight",
+                "AS_ODSTSniperLeft",
+                "AS_ODSTSniperRight",
+                "AS_SmallLeft",
+                "AS_SmallRight",
+                "AP_AR",
+                "AP_BR",
+                "AP_Canteen",
+                "AP_Knife",
+                "AP_MGThigh",
+                "AP_AR",
+                "AP_Pack",
+                "AP_Pistol",
+                "AP_Rounds",
+                "AP_SG",
+                "AP_SMG",
+                "AP_Sniper",
+                "AP_Thigh",
+                "APO_BR",
+                "APO_Knife",
+                "APO_SMG",
+                "APO_Sniper",
+                "CustomKit_Scorch"
+            };
+        };
+    };
+    /* Green */
+    class KAT_M56S_Jaeger_Armor_Rifleman_Green : KAT_M56S_Jaeger_Armor_Rifleman { };
+    class KAT_M56S_Jaeger_Armor_Grenadier_Green : KAT_M56S_Jaeger_Armor_Rifleman_Green
+    {
+        displayName = "[KAT] M56S/J Grenadier [Green]";
+        hiddenSelections[] =
+        {
+            "camo",
+            "camo2",
+            "camo3",
+            "camo4",
+            "camo5",
+            "A_KneesMarLeft",
+            "A_KneesMarRight",
+            "A_Ghillie",
+            "A_TacPad",
+            "AS_LargeLeft",
+            "AS_LargeRight",
+            "AS_MediumLeft",
+            "AS_MediumRight",
+            "AS_ODSTCQBLeft",
+            "AS_ODSTCQBRight",
+            "AS_ODSTSniperLeft",
+            "AS_ODSTSniperRight",
+            "AS_SmallLeft",
+            "AS_SmallRight",
+            "AP_AR",
+            "AP_BR",
+            "AP_Canteen",
+            "AP_Knife",
+            "AP_MGThigh",
+            "AP_AR",
+            "AP_Pack",
+            "AP_Pistol",
+            "AP_Rounds",
+            "AP_SG",
+            "AP_SMG",
+            "AP_Sniper",
+            "AP_Thigh",
+            "APO_BR",
+            "APO_Knife",
+            "APO_SMG",
+            "APO_Sniper",
+            "CustomKit_Scorch"
+        };
+        class ItemInfo: ItemInfo
+        {
+            hiddenSelections[] =
+            {
+                "camo",
+                "camo2",
+                "camo3",
+                "camo4",
+                "camo5",
+                "A_KneesMarLeft",
+                "A_KneesMarRight",
+                "A_Ghillie",
+                "A_TacPad",
+                "AS_LargeLeft",
+                "AS_LargeRight",
+                "AS_MediumLeft",
+                "AS_MediumRight",
+                "AS_ODSTCQBLeft",
+                "AS_ODSTCQBRight",
+                "AS_ODSTSniperLeft",
+                "AS_ODSTSniperRight",
+                "AS_SmallLeft",
+                "AS_SmallRight",
+                "AP_AR",
+                "AP_BR",
+                "AP_Canteen",
+                "AP_Knife",
+                "AP_MGThigh",
+                "AP_AR",
+                "AP_Pack",
+                "AP_Pistol",
+                "AP_Rounds",
+                "AP_SG",
+                "AP_SMG",
+                "AP_Sniper",
+                "AP_Thigh",
+                "APO_BR",
+                "APO_Knife",
+                "APO_SMG",
+                "APO_Sniper",
+                "CustomKit_Scorch"
+            };
+        };
+    };
+    /* Yellow */
+    class KAT_M56S_Jaeger_Armor_Rifleman_Yellow : KAT_M56S_Jaeger_Armor_Rifleman { };
+    class KAT_M56S_Jaeger_Armor_Grenadier_Yellow : KAT_M56S_Jaeger_Armor_Rifleman_Yellow
+    {
+        displayName = "[KAT] M56S/J Grenadier [Yellow]";
+        hiddenSelections[] =
+        {
+            "camo",
+            "camo2",
+            "camo3",
+            "camo4",
+            "camo5",
+            "A_KneesMarLeft",
+            "A_KneesMarRight",
+            "A_Ghillie",
+            "A_TacPad",
+            "AS_LargeLeft",
+            "AS_LargeRight",
+            "AS_MediumLeft",
+            "AS_MediumRight",
+            "AS_ODSTCQBLeft",
+            "AS_ODSTCQBRight",
+            "AS_ODSTSniperLeft",
+            "AS_ODSTSniperRight",
+            "AS_SmallLeft",
+            "AS_SmallRight",
+            "AP_AR",
+            "AP_BR",
+            "AP_Canteen",
+            "AP_Knife",
+            "AP_MGThigh",
+            "AP_AR",
+            "AP_Pack",
+            "AP_Pistol",
+            "AP_Rounds",
+            "AP_SG",
+            "AP_SMG",
+            "AP_Sniper",
+            "AP_Thigh",
+            "APO_BR",
+            "APO_Knife",
+            "APO_SMG",
+            "APO_Sniper",
+            "CustomKit_Scorch"
+        };
+        class ItemInfo: ItemInfo
+        {
+            hiddenSelections[] =
+            {
+                "camo",
+                "camo2",
+                "camo3",
+                "camo4",
+                "camo5",
+                "A_KneesMarLeft",
+                "A_KneesMarRight",
+                "A_Ghillie",
+                "A_TacPad",
+                "AS_LargeLeft",
+                "AS_LargeRight",
+                "AS_MediumLeft",
+                "AS_MediumRight",
+                "AS_ODSTCQBLeft",
+                "AS_ODSTCQBRight",
+                "AS_ODSTSniperLeft",
+                "AS_ODSTSniperRight",
+                "AS_SmallLeft",
+                "AS_SmallRight",
+                "AP_AR",
+                "AP_BR",
+                "AP_Canteen",
+                "AP_Knife",
+                "AP_MGThigh",
+                "AP_AR",
+                "AP_Pack",
+                "AP_Pistol",
+                "AP_Rounds",
+                "AP_SG",
+                "AP_SMG",
+                "AP_Sniper",
+                "AP_Thigh",
+                "APO_BR",
+                "APO_Knife",
+                "APO_SMG",
+                "APO_Sniper",
+                "CustomKit_Scorch"
+            };
+        };
+    };
+    /* Purple */
+    class KAT_M56S_Jaeger_Armor_Rifleman_Purple : KAT_M56S_Jaeger_Armor_Rifleman { };
+    class KAT_M56S_Jaeger_Armor_Grenadier_Purple : KAT_M56S_Jaeger_Armor_Rifleman_Purple
+    {
+        displayName = "[KAT] M56S/J Grenadier [Purple]";
+        hiddenSelections[] =
+        {
+            "camo",
+            "camo2",
+            "camo3",
+            "camo4",
+            "camo5",
+            "A_KneesMarLeft",
+            "A_KneesMarRight",
+            "A_Ghillie",
+            "A_TacPad",
+            "AS_LargeLeft",
+            "AS_LargeRight",
+            "AS_MediumLeft",
+            "AS_MediumRight",
+            "AS_ODSTCQBLeft",
+            "AS_ODSTCQBRight",
+            "AS_ODSTSniperLeft",
+            "AS_ODSTSniperRight",
+            "AS_SmallLeft",
+            "AS_SmallRight",
+            "AP_AR",
+            "AP_BR",
+            "AP_Canteen",
+            "AP_Knife",
+            "AP_MGThigh",
+            "AP_AR",
+            "AP_Pack",
+            "AP_Pistol",
+            "AP_Rounds",
+            "AP_SG",
+            "AP_SMG",
+            "AP_Sniper",
+            "AP_Thigh",
+            "APO_BR",
+            "APO_Knife",
+            "APO_SMG",
+            "APO_Sniper",
+            "CustomKit_Scorch"
+        };
+        class ItemInfo: ItemInfo
+        {
+            hiddenSelections[] =
+            {
+                "camo",
+                "camo2",
+                "camo3",
+                "camo4",
+                "camo5",
+                "A_KneesMarLeft",
+                "A_KneesMarRight",
+                "A_Ghillie",
+                "A_TacPad",
+                "AS_LargeLeft",
+                "AS_LargeRight",
+                "AS_MediumLeft",
+                "AS_MediumRight",
+                "AS_ODSTCQBLeft",
+                "AS_ODSTCQBRight",
+                "AS_ODSTSniperLeft",
+                "AS_ODSTSniperRight",
+                "AS_SmallLeft",
+                "AS_SmallRight",
+                "AP_AR",
+                "AP_BR",
+                "AP_Canteen",
+                "AP_Knife",
+                "AP_MGThigh",
+                "AP_AR",
+                "AP_Pack",
+                "AP_Pistol",
+                "AP_Rounds",
+                "AP_SG",
+                "AP_SMG",
+                "AP_Sniper",
+                "AP_Thigh",
+                "APO_BR",
+                "APO_Knife",
+                "APO_SMG",
+                "APO_Sniper",
+                "CustomKit_Scorch"
+            };
+        };
+    };
+};
