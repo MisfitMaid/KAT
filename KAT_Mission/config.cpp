baa78a7345e8ad26a08b0c70e6291216ad1b8a99
Keira Dueck <sylae@calref.net>
2023-12-17 08:25:23 -0800

    mission assets pbo

---
+++ KAT_Mission/config.cpp
@@ -0,0 +1,17 @@
+#define _ARMA_
+
+// Long-term mission assets, no sense in making the user redownload them every mission file change.
+class CfgPatches
+{
+	class KAT_Mission
+	{
+		author = "MisfitMaid";
+		hideName = 0;
+		units[] = {
+		};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {
+		};
+	};
+};
