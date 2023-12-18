0f194279ad747b7ef6d688d056887e651852d1cc
Keira Dueck <sylae@calref.net>
2023-12-18 11:11:04 -0800

    i hate caching i hate caching

--- build.ps1
+++ build.ps1
@@ -6,6 +6,9 @@ if ($args.count -gt 0) {
 	$dirs = "KAT_*";
 }
 
+Remove-Item "temp" -Force  -Recurse -ErrorAction SilentlyContinue
+mkdir "temp";
+
 # preprocessor stuff
 $oldPreference = $ErrorActionPreference
 $ErrorActionPreference = 'stop'
@@ -13,7 +16,7 @@ try {if(Get-Command php){
 	php KAT_ARMOR/_preprocessor/makeArmor.php
 	php KAT_Gear/tictacs/csvToTictac.php
 }}
-catch {Write-Host "[WARN] php not found. skipping tictac generation" -ForegroundColor yellow}
+catch {Write-Host "[WARN] php not found. skipping armor and tictac generation" -ForegroundColor yellow}
 finally {$ErrorActionPreference = $oldPreference}
 
 Get-ChildItem . -Directory $dirs | Foreach-Object {
@@ -21,5 +24,5 @@ Get-ChildItem . -Directory $dirs | Foreach-Object {
 	$signArg = "-sign=" + $privKey;
 	$whitelistArg = "-include=" + $( Get-Location ) + "\addonBuilderWhitelist.txt";
 	
-	& $addonBuilder $fn $destination $signArg $whitelistArg -binarizeFullLogs -binarizeAllTextures;
+	& $addonBuilder $fn $destination $signArg $whitelistArg -binarizeFullLogs -binarizeAllTextures -clear -temp=temp;
 }
