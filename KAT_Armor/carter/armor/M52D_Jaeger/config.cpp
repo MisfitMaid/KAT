6036dddcffdfc72e16f3fb7c7a44fa5162c45025
jmack <jmack.contact@gmail.com>
2023-12-31 12:36:43 -0600

    Add snow variant of Carter custom M52D

--- KAT_Armor/carter/armor/M52D_Jaeger/config.cpp
+++ KAT_Armor/carter/armor/M52D_Jaeger/config.cpp
@@ -95,4 +95,15 @@ class CfgWeapons
             };
         };
     };
+    class KAT_Armor_M52D_Carter_Snow : KAT_M52D_Jaeger_Armor_Carter_Berserker
+    {
+        hiddenSelectionsTextures[] =
+        {
+            "KAT_Armor\_base\armor\M52D_Jaeger\_textures\Vest\vest_odst_snow.paa",
+            "KAT_Armor\_base\armor\M52D_Jaeger\_textures\Armor\armor_odst_snow.paa",
+            "KAT_Armor\_base\armor\M52D_Jaeger\_textures\Legs\Snow\legs_berserker.paa",
+            "optre_unsc_units\army\data\ghillie_desert_co.paa",
+            "KAT_Armor\carter\armor\M52D_Jaeger\_textures\main_berserker_snow.paa",
+        };
+    };
 };
