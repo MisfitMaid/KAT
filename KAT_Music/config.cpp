58fd24a7e1eb6b08a7b019db0b921fae1c20ea01
Keira Dueck <sylae@calref.net>
2024-01-13 02:26:13 -0800

    halo infinite soundtrack

---
+++ KAT_Music/config.cpp
@@ -0,0 +1,462 @@
+class CfgPatches
+{
+	class KAT_Music
+	{
+		author = "MisfitMaid";
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+		units[] = {};
+		weapons[] = {};
+      	worlds[] = {};
+	};
+};
+class CfgMusic
+{
+	class KAT_Music_track000
+	{
+		name = "01 Set a Fire in Your Heart";
+		sound[] = {"KAT_Music\tracks\01 Set a Fire in Your Heart.ogg",db+0,1};
+		duration=140;
+		musicClass = "KAT_Music_INF_Fire";
+	};
+	class KAT_Music_track001
+	{
+		name = "02 Reverie";
+		sound[] = {"KAT_Music\tracks\02 Reverie.ogg",db+0,1};
+		duration=225;
+		musicClass = "KAT_Music_INF_Fire";
+	};
+	class KAT_Music_track002
+	{
+		name = "03 Through the Trees";
+		sound[] = {"KAT_Music\tracks\03 Through the Trees.ogg",db+0,1};
+		duration=192;
+		musicClass = "KAT_Music_INF_Fire";
+	};
+	class KAT_Music_track003
+	{
+		name = "01. Zeta Halo";
+		sound[] = {"KAT_Music\tracks\01. Zeta Halo.ogg",db+0,1};
+		duration=211;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track004
+	{
+		name = "02. Sacrifice";
+		sound[] = {"KAT_Music\tracks\02. Sacrifice.ogg",db+0,1};
+		duration=97;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track005
+	{
+		name = "03. The Banished";
+		sound[] = {"KAT_Music\tracks\03. The Banished.ogg",db+0,1};
+		duration=165;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track006
+	{
+		name = "04. Gbraakon Escape";
+		sound[] = {"KAT_Music\tracks\04. Gbraakon Escape.ogg",db+0,1};
+		duration=149;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track007
+	{
+		name = "05. Escharum";
+		sound[] = {"KAT_Music\tracks\05. Escharum.ogg",db+0,1};
+		duration=256;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track008
+	{
+		name = "06. Follow the Signal";
+		sound[] = {"KAT_Music\tracks\06. Follow the Signal.ogg",db+0,1};
+		duration=135;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track009
+	{
+		name = "07. Foundations";
+		sound[] = {"KAT_Music\tracks\07. Foundations.ogg",db+0,1};
+		duration=204;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track010
+	{
+		name = "08. The Weapon";
+		sound[] = {"KAT_Music\tracks\08. The Weapon.ogg",db+0,1};
+		duration=131;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track011
+	{
+		name = "09. Know My Legend";
+		sound[] = {"KAT_Music\tracks\09. Know My Legend.ogg",db+0,1};
+		duration=136;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track012
+	{
+		name = "10. Reverie";
+		sound[] = {"KAT_Music\tracks\10. Reverie.ogg",db+0,1};
+		duration=220;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track013
+	{
+		name = "11. The Road";
+		sound[] = {"KAT_Music\tracks\11. The Road.ogg",db+0,1};
+		duration=126;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track014
+	{
+		name = "12. Echo-216";
+		sound[] = {"KAT_Music\tracks\12. Echo-216.ogg",db+0,1};
+		duration=203;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track015
+	{
+		name = "13. Ransom Keep";
+		sound[] = {"KAT_Music\tracks\13. Ransom Keep.ogg",db+0,1};
+		duration=194;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track016
+	{
+		name = "14. Tower";
+		sound[] = {"KAT_Music\tracks\14. Tower.ogg",db+0,1};
+		duration=211;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track017
+	{
+		name = "15. Through the Trees";
+		sound[] = {"KAT_Music\tracks\15. Through the Trees.ogg",db+0,1};
+		duration=188;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track018
+	{
+		name = "16. Horn of Abolition";
+		sound[] = {"KAT_Music\tracks\16. Horn of Abolition.ogg",db+0,1};
+		duration=161;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track019
+	{
+		name = "17. Excavation Site";
+		sound[] = {"KAT_Music\tracks\17. Excavation Site.ogg",db+0,1};
+		duration=243;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track020
+	{
+		name = "18. Conservatory";
+		sound[] = {"KAT_Music\tracks\18. Conservatory.ogg",db+0,1};
+		duration=174;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track021
+	{
+		name = "19. Endless";
+		sound[] = {"KAT_Music\tracks\19. Endless.ogg",db+0,1};
+		duration=262;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track022
+	{
+		name = "20. Spire";
+		sound[] = {"KAT_Music\tracks\20. Spire.ogg",db+0,1};
+		duration=229;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track023
+	{
+		name = "21. Adjutant Resolution";
+		sound[] = {"KAT_Music\tracks\21. Adjutant Resolution.ogg",db+0,1};
+		duration=210;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track024
+	{
+		name = "22. Pelican Down";
+		sound[] = {"KAT_Music\tracks\22. Pelican Down.ogg",db+0,1};
+		duration=62;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track025
+	{
+		name = "23. Heavy Artillery";
+		sound[] = {"KAT_Music\tracks\23. Heavy Artillery.ogg",db+0,1};
+		duration=185;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track026
+	{
+		name = "24. Scattered, Hunted, Defeated.";
+		sound[] = {"KAT_Music\tracks\24. Scattered, Hunted, Defeated..ogg",db+0,1};
+		duration=129;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track027
+	{
+		name = "25. Seeing Phantoms";
+		sound[] = {"KAT_Music\tracks\25. Seeing Phantoms.ogg",db+0,1};
+		duration=152;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track028
+	{
+		name = "26. What Makes Us Human";
+		sound[] = {"KAT_Music\tracks\26. What Makes Us Human.ogg",db+0,1};
+		duration=177;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track029
+	{
+		name = "27. Under Cover";
+		sound[] = {"KAT_Music\tracks\27. Under Cover.ogg",db+0,1};
+		duration=208;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track030
+	{
+		name = "28. Riven Gate";
+		sound[] = {"KAT_Music\tracks\28. Riven Gate.ogg",db+0,1};
+		duration=167;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track031
+	{
+		name = "29. Sequence";
+		sound[] = {"KAT_Music\tracks\29. Sequence.ogg",db+0,1};
+		duration=169;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track032
+	{
+		name = "30. Nexus";
+		sound[] = {"KAT_Music\tracks\30. Nexus.ogg",db+0,1};
+		duration=71;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track033
+	{
+		name = "31. Command Spire";
+		sound[] = {"KAT_Music\tracks\31. Command Spire.ogg",db+0,1};
+		duration=222;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track034
+	{
+		name = "32. Control Room";
+		sound[] = {"KAT_Music\tracks\32. Control Room.ogg",db+0,1};
+		duration=176;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track035
+	{
+		name = "33. Three, Two, One.";
+		sound[] = {"KAT_Music\tracks\33. Three, Two, One..ogg",db+0,1};
+		duration=153;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track036
+	{
+		name = "34. Repository";
+		sound[] = {"KAT_Music\tracks\34. Repository.ogg",db+0,1};
+		duration=129;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track037
+	{
+		name = "35. Chapel";
+		sound[] = {"KAT_Music\tracks\35. Chapel.ogg",db+0,1};
+		duration=175;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track038
+	{
+		name = "36. Imprisoned";
+		sound[] = {"KAT_Music\tracks\36. Imprisoned.ogg",db+0,1};
+		duration=120;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track039
+	{
+		name = "37. Bridge Too Far";
+		sound[] = {"KAT_Music\tracks\37. Bridge Too Far.ogg",db+0,1};
+		duration=158;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track040
+	{
+		name = "38. We Do It Together";
+		sound[] = {"KAT_Music\tracks\38. We Do It Together.ogg",db+0,1};
+		duration=239;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track041
+	{
+		name = "39. Never Tell Me the Odds";
+		sound[] = {"KAT_Music\tracks\39. Never Tell Me the Odds.ogg",db+0,1};
+		duration=183;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track042
+	{
+		name = "40. House of Reckoning";
+		sound[] = {"KAT_Music\tracks\40. House of Reckoning.ogg",db+0,1};
+		duration=231;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track043
+	{
+		name = "41. Test of Mettle";
+		sound[] = {"KAT_Music\tracks\41. Test of Mettle.ogg",db+0,1};
+		duration=271;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track044
+	{
+		name = "42. There Will Be Consequences";
+		sound[] = {"KAT_Music\tracks\42. There Will Be Consequences.ogg",db+0,1};
+		duration=136;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track045
+	{
+		name = "43. Palace Arrival";
+		sound[] = {"KAT_Music\tracks\43. Palace Arrival.ogg",db+0,1};
+		duration=181;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track046
+	{
+		name = "44. Silent Auditorium";
+		sound[] = {"KAT_Music\tracks\44. Silent Auditorium.ogg",db+0,1};
+		duration=284;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track047
+	{
+		name = "45. Judgment";
+		sound[] = {"KAT_Music\tracks\45. Judgment.ogg",db+0,1};
+		duration=264;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track048
+	{
+		name = "46. A Message";
+		sound[] = {"KAT_Music\tracks\46. A Message.ogg",db+0,1};
+		duration=210;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track049
+	{
+		name = "47. Finale";
+		sound[] = {"KAT_Music\tracks\47. Finale.ogg",db+0,1};
+		duration=198;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track050
+	{
+		name = "48. Hunter's Dance";
+		sound[] = {"KAT_Music\tracks\48. Hunter's Dance.ogg",db+0,1};
+		duration=193;
+		musicClass = "KAT_Music_INF_Camp";
+	};
+	class KAT_Music_track051
+	{
+		name = "01. What is a Spartan";
+		sound[] = {"KAT_Music\tracks\01. What is a Spartan.ogg",db+0,1};
+		duration=148;
+		musicClass = "KAT_Music_INF_Mult";
+	};
+	class KAT_Music_track052
+	{
+		name = "02. The Precipice of Power";
+		sound[] = {"KAT_Music\tracks\02. The Precipice of Power.ogg",db+0,1};
+		duration=201;
+		musicClass = "KAT_Music_INF_Mult";
+	};
+	class KAT_Music_track053
+	{
+		name = "03. Forged in Fire";
+		sound[] = {"KAT_Music\tracks\03. Forged in Fire.ogg",db+0,1};
+		duration=252;
+		musicClass = "KAT_Music_INF_Mult";
+	};
+	class KAT_Music_track054
+	{
+		name = "04. Taking Back What's Ours";
+		sound[] = {"KAT_Music\tracks\04. Taking Back What's Ours.ogg",db+0,1};
+		duration=236;
+		musicClass = "KAT_Music_INF_Mult";
+	};
+	class KAT_Music_track055
+	{
+		name = "05. Of Blood and Burden";
+		sound[] = {"KAT_Music\tracks\05. Of Blood and Burden.ogg",db+0,1};
+		duration=230;
+		musicClass = "KAT_Music_INF_Mult";
+	};
+	class KAT_Music_track056
+	{
+		name = "06. No Greater Alliance";
+		sound[] = {"KAT_Music\tracks\06. No Greater Alliance.ogg",db+0,1};
+		duration=219;
+		musicClass = "KAT_Music_INF_Mult";
+	};
+	class KAT_Music_track057
+	{
+		name = "07. Heroes. Legends. Immortal.";
+		sound[] = {"KAT_Music\tracks\07. Heroes. Legends. Immortal..ogg",db+0,1};
+		duration=219;
+		musicClass = "KAT_Music_INF_Mult";
+	};
+	class KAT_Music_track058
+	{
+		name = "08. Though This be Madness";
+		sound[] = {"KAT_Music\tracks\08. Though This be Madness.ogg",db+0,1};
+		duration=195;
+		musicClass = "KAT_Music_INF_Mult";
+	};
+	class KAT_Music_track059
+	{
+		name = "09. Augmented Realities";
+		sound[] = {"KAT_Music\tracks\09. Augmented Realities.ogg",db+0,1};
+		duration=253;
+		musicClass = "KAT_Music_INF_Mult";
+	};
+	class KAT_Music_track060
+	{
+		name = "10. Between Collapse and Control";
+		sound[] = {"KAT_Music\tracks\10. Between Collapse and Control.ogg",db+0,1};
+		duration=224;
+		musicClass = "KAT_Music_INF_Mult";
+	};
+	class KAT_Music_track061
+	{
+		name = "11. Echoes of Orion";
+		sound[] = {"KAT_Music\tracks\11. Echoes of Orion.ogg",db+0,1};
+		duration=230;
+		musicClass = "KAT_Music_INF_Mult";
+	};
+	class KAT_Music_track062
+	{
+		name = "12. Legacy of Generations";
+		sound[] = {"KAT_Music\tracks\12. Legacy of Generations.ogg",db+0,1};
+		duration=219;
+		musicClass = "KAT_Music_INF_Mult";
+	};
+};
+class CfgMusicClasses
+{
+	class KAT_Music_INF_Camp { displayName = "Halo Infinite [Campaign]"; };
+	class KAT_Music_INF_Fire { displayName = "Halo Infinite [Set A Fire In Your Heart EP]"; };
+	class KAT_Music_INF_Mult { displayName = "Halo Infinite [Multiplayer]"; };
+};
