7af1710a91cc0d72131ad05a9dc9fa99088f6fe7
Keira Dueck <sylae@calref.net>
2024-04-02 16:01:10 -0700

    jamison armor

---
+++ KAT_Mjolnir/G258-Jamison/config.cpp
@@ -0,0 +1,157 @@
+class CfgPatches
+{
+	class KAT_Mjolnir_G258
+	{
+		author = "MisfitMaid";
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] =
+		{
+			"A3_Characters_F",
+			"OPTRE_Core"
+		};
+	};
+};
+
+class CfgWeapons
+{
+    class MAPO_MKV_Helmet;
+    class KAT_Mjolnir_Helmet_G258 : MAPO_MKV_Helmet
+	{
+		MJOLNIR_isHelmet=1;
+		scope = 2;
+		scopeArsenal = 2;
+		displayName = "[Azrael] Mjolnir Helmet G258";
+		hiddenSelections[]=
+		{
+			"camo1",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"MA_Armor\data\Helmets\MKV\helmets\black_mkv_helmet_co.paa",
+			"MA_Armor\data\Helmets\MKV\visors\black_mkv_visor_co.paa"
+		};
+	};
+
+	class MAPO_MKVB_Armor;
+	class KAT_Mjolnir_Vest_G258 : MAPO_MKVB_Armor
+	{
+		MJOLNIR_isArmor=1;
+		scope = 2;
+		scopeArsenal = 2;
+		displayName = "[Azrael] Mjolnir Armor G258";
+		hiddenSelections[]=
+		{
+			"camo1",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"MA_Armor\data\Vests\MKVB\color_variants\black_mkv_upper_co.paa",
+			"MA_Armor\data\Vests\MKVB\color_variants\black_mkv_lower_co.paa"
+		};
+	};
+};
+
+class CfgVehicles
+{
+	class Mjolnir_Recon_Shoulders_Spartan;
+	class KAT_Mjolnir_Shoulders_G258: Mjolnir_Recon_Shoulders_Spartan
+	{
+		scope=2;
+		scopeArsenal=2;
+		displayName="[Azrael] Mjolnir Shoulders G258";
+
+		hiddenSelections[]=
+		{
+			"camo1",
+			"camo2",
+			"camo3",
+			"camo4",
+			"camo5",
+			"camo6",
+			"camo7",
+			"camo8",
+			"camo9",
+			"camo10",
+			"camo11",
+			"camo12",
+			"camo13",
+			"camo14",
+			"camo15",
+			"camo16",
+			"camo17",
+			"camo18",
+			"camo19",
+			"camo20",
+			"camo21",
+			"camo22",
+			"camo23",
+			"camo24",
+			"camo25",
+			"camo26",
+			"camo27",
+			"camo28",
+			"commando_left",
+			"commando_right",
+			"cqc_left",
+			"cqc_right",
+			"eva_left",
+			"eva_right",
+			"gren_left",
+			"gren_right",
+			"gungnir_left",
+			"gungnir_right",
+			"hayabusa_left",
+			"hayabusa_right",
+			"jfo_left",
+			"jfo_right",
+			// "mkiv_left",
+			// "mkiv_right",
+			"mkv_left",
+			"mkv_right",
+			"odst_left",
+			"odst_right",
+			"operator_left",
+			"operator_right",
+			"recon_left",
+			"recon_right",
+			"security_left",
+			"security_right",
+			"scout_left",
+			"scout_right"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"MA_Armor\data\Vests\MKVB\Shoulders\Commando\Olive_Commando_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\Commando\Olive_Commando_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\CQC\Olive_CQC_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\CQC\Olive_CQC_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\EVA\Olive_EVA_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\EVA\Olive_EVA_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\Grenadier\Olive_Grenadier_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\Grenadier\Olive_Grenadier_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\Gungnir\Olive_GUNGNIR_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\Gungnir\Olive_GUNGNIR_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\Hayabusa\Olive_Hayabusa_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\Hayabusa\Olive_Hayabusa_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\JFO\Olive_JFO_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\JFO\Olive_JFO_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKIV\Color_Variants\Black_MKIV_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKIV\Color_Variants\Black_MKIV_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\MKV\Olive_MKV_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\MKV\Olive_MKV_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\ODST\Olive_ODST_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\ODST\Olive_ODST_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\Operator\Olive_Operator_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\Operator\Olive_Operator_Shoulders_CO.paa",
+			"KAT_Mjolnir\G258-Tessa\shoulders.paa",
+			"KAT_Mjolnir\G258-Tessa\shoulders.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\Security\Olive_Security_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\Security\Olive_Security_Shoulders_CO.paa",
+			"MA_Armor\data\Vests\MKVB\Shoulders\Scout\Olive_Scout_Shoulders_CO.paa"
+		};
+	};
+};
