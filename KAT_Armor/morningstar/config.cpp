4e0199eab31da951d39acd06172d57f42e632f7b
Keira Dueck <sylae@calref.net>
2024-04-23 18:49:39 -0700

    morningstar

--- KAT_Armor/morningstar/config.cpp
+++ KAT_Armor/morningstar/config.cpp
@@ -18,13 +18,14 @@ class CfgWeapons
 {
     class ItemInfo;
 
+	class KAT_M56SR_Helmet_Base;
     class KAT_Armor__Base;
     class KAT_Armor_Morningstar : KAT_Armor__Base
 	{
 		scope = 2;
 		scopeArsenal = 2;
-		author = "MisfitMaid";
-		displayName = "[Azrael] M56S-H (Morningstar)";
+		author = "frwrd";
+		displayName = "[Azrael] M56S-R (Morningstar)";
 		hiddenSelections[] = 
 		{
 			"camo1",
@@ -37,14 +38,14 @@ class CfgWeapons
 			"camo8",
 			"camo9",
 			"camo10",
-			"Reach_Forearm_Left",
-			"Reach_Forearm_Right",
-			//"H3_Forearm_Left",
-			//"H3_Forearm_Right",
-			//"H3_Pauldron_Left",
-			//"H3_Pauldron_Right",
-			"Reach_Pauldron_Left",
-			"Reach_Pauldron_Right",
+			//"Reach_Forearm_Left",
+			//"Reach_Forearm_Right",
+			"H3_Forearm_Left",
+			"H3_Forearm_Right",
+			"H3_Pauldron_Left",
+			"H3_Pauldron_Right",
+			//"Reach_Pauldron_Left",
+			//"Reach_Pauldron_Right",
 			"Reach_Shoulder_Radio_Left",
 			"Reach_Shoulder_Radio_Right",
 			"Reach_Sniper_Pauldron_Left",
@@ -52,19 +53,26 @@ class CfgWeapons
 			"Reach_CQB_Pauldron_Left",
 			"Reach_CQB_Pauldron_Right",
 			"Thigh_Pouch",
-			"Reach_Armor_Upper",
+			//"Reach_Armor_Upper",
 			"Reach_Armor_Lower",
-			//"H3_Armor_Upper",
+			"H3_Armor_Upper",
 			//"H3_Armor_Lower",
 			"Forearm_Vent_Left",
 			"Forearm_Vent_Right",
 			"Canisters"
 		};
-
 		hiddenSelectionsTextures[] = 
 		{
-			"KAT_Armor\morningstar\H3_Upper_Armor_co.paa",
-			"KAT_Armor\morningstar\H3_Lower_Armor_co.paa"
+			"",       //camo1 (H3 Upper Armor)
+			"KAT_Armor\morningstar\lower.paa",       //camo2 (H3 Lower Armor)
+			"KAT_Armor\morningstar\thigh.paa",//camo3 (Thigh Pouch)
+			"KAT_Armor\morningstar\upper.paa",    //camo4 (Reach Upper Armor)
+			"",    //camo5 (Reach Lower Armor)
+			"KAT_Armor\morningstar\cans.paa",      //camo6 (Canisters)
+			"",   //camo7 (Forearm Vents)
+			"", //camo8 (Sniper Shoulders)
+			"",  //camo9 (CQB Shoulders)
+			"" //camo10 (Shoulder Radio)
 		};
 		class ItemInfo: ItemInfo
 		{
@@ -83,14 +91,14 @@ class CfgWeapons
 				"camo8",
 				"camo9",
 				"camo10",
-				"Reach_Forearm_Left",
-				"Reach_Forearm_Right",
-				//"H3_Forearm_Left",
-				//"H3_Forearm_Right",
-				//"H3_Pauldron_Left",
-				//"H3_Pauldron_Right",
-				"Reach_Pauldron_Left",
-				"Reach_Pauldron_Right",
+				//"Reach_Forearm_Left",
+				//"Reach_Forearm_Right",
+				"H3_Forearm_Left",
+				"H3_Forearm_Right",
+				"H3_Pauldron_Left",
+				"H3_Pauldron_Right",
+				//"Reach_Pauldron_Left",
+				//"Reach_Pauldron_Right",
 				"Reach_Shoulder_Radio_Left",
 				"Reach_Shoulder_Radio_Right",
 				"Reach_Sniper_Pauldron_Left",
@@ -98,37 +106,43 @@ class CfgWeapons
 				"Reach_CQB_Pauldron_Left",
 				"Reach_CQB_Pauldron_Right",
 				"Thigh_Pouch",
-				"Reach_Armor_Upper",
+				//"Reach_Armor_Upper",
 				"Reach_Armor_Lower",
-				//"H3_Armor_Upper",
+				"H3_Armor_Upper",
 				//"H3_Armor_Lower",
 				"Forearm_Vent_Left",
 				"Forearm_Vent_Right",
 				"Canisters"
 			};
-
 			hiddenSelectionsTextures[] = 
 			{
-				"KAT_Armor\morningstar\H3_Upper_Armor_co.paa",
-				"KAT_Armor\morningstar\H3_Lower_Armor_co.paa"
+				"",       //camo1 (H3 Upper Armor)
+				"KAT_Armor\morningstar\lower.paa",       //camo2 (H3 Lower Armor)
+				"KAT_Armor\morningstar\thigh.paa",//camo3 (Thigh Pouch)
+				"KAT_Armor\morningstar\upper.paa",    //camo4 (Reach Upper Armor)
+				"",    //camo5 (Reach Lower Armor)
+				"KAT_Armor\morningstar\cans.paa",      //camo6 (Canisters)
+				"",   //camo7 (Forearm Vents)
+				"", //camo8 (Sniper Shoulders)
+				"",  //camo9 (CQB Shoulders)
+				"" //camo10 (Shoulder Radio)
 			};
 		};
 	};
 
-	class KAT_M56SR_Helmet_Base;
-	class KAT_Helmets_Morningstar: KAT_M56SR_Helmet_Base
+    class KAT_Helmets_Morningstar: KAT_M56SR_Helmet_Base
 	{
 		scope = 2;
 		scopeCurator = 2;
 		scopeArsenal = 2;
-		author = "MisfitMaid";
-		displayName = "[Azrael] M56R (Morningstar)";
+		author = "frwrd";
+		displayName = "[Azrael] M56S-R (Morningstar)";
 		hiddenSelections[] = {"camo1","camo2"};
-		hiddenSelectionsTextures[] = {"KAT_Armor\morningstar\hr_odst_helmet_co.paa","MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_Red_co.paa"};
+		hiddenSelectionsTextures[] = {"KAT_Armor\morningstar\helmet.paa","KAT_Armor\morningstar\visor.paa"};
 		class ItemInfo: ItemInfo
 		{
 			hiddenSelections[] = {"camo1","camo2"};
-			hiddenSelectionsTextures[] = {"KAT_Armor\morningstar\hr_odst_helmet_co.paa","MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_Red_co.paa"};
+			hiddenSelectionsTextures[] = {"KAT_Armor\morningstar\helmet.paa","KAT_Armor\morningstar\visor.paa"};
 		};
 	};
 };
