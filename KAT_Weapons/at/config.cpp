ee4123cbe161a50df864ea6e55fe73ec3f113ab1
Keira Dueck <sylae@calref.net>
2024-12-15 17:10:59 -0800

    KAT_Weapons stub classes

---
+++ KAT_Weapons/at/config.cpp
@@ -0,0 +1,16 @@
+#define _ARMA_
+
+class CfgPatches
+{
+	class KAT_Weapons_AT
+	{
+		author = "Azrael Quartermaster Team";
+		hideName = 0;
+		units[] = {
+		};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {
+		};
+	};
+};
